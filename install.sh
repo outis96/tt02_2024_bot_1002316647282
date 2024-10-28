80736d92ed719183aaad0ffa65961820543db247
khuong-nv <khuong96.vn@gmail.com>
2024-10-28 23:29:59 +0700

    share project

---
+++ install.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+git clone https://github.com/outis96/tt02_2024_bot_1002316647282.git
+cd tt02_2024_bot_1002316647282
+./start_docker.sh &
